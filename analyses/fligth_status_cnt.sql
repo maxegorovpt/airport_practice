bf118600122908928f4d34b15002b43cd0e31f3c
Maxim Egorov <maxim.egorov@fundraiseup.com>
2026-06-06 22:23:21 +0100

    jinja commit 2

--- analyses/fligth_status_cnt.sql
+++ analyses/fligth_status_cnt.sql
@@ -1,5 +1,18 @@
-select 
-    status,
-    count(*)
+{% set status_query%}
+select distinct status
 from {{ ref('stg_flights__flights') }}
-group by status
+{% endset %}
+
+{% set status_query_result = run_query(status_query)%}
+{% if execute%}
+    {%set all_statuses = status_query_result.columns[0].values()%}
+{% else %}
+    {% set all_statuses = [] %}
+{% endif%}
+
+select 
+    {% for status_name in all_statuses%}
+    sum(case when status = '{{ status_name}}' then 1 else 0 end) as status_{{status_name.replace(" ", "_")}}
+        {%- if not loop.last %},{% endif%}
+    {%- endfor%}
+from {{ ref('stg_flights__flights') }}
