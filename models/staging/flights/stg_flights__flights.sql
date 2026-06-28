b0c18ccd130417473df0356918cd8e3836b9181e
Maxim Egorov <maxim.egorov@fundraiseup.com>
2026-06-28 22:28:53 +0100

    dbt model generates

--- models/staging/flights/stg_flights__flights.sql
+++ models/staging/flights/stg_flights__flights.sql
@@ -1,24 +1,25 @@
-{{
-      config(
-        materialized = 'incremental',
-        incremental_strategy = 'merge',
-        unique_key = 'flight_id',
-        tag = 'staging'
-    )
-}}
-select 
-    flight_id,
-    flight_no,
-    scheduled_departure,
-    scheduled_arrival,
-    departure_airport,
-    arrival_airport,
-    status,
-    aircraft_code,
-    actual_departure,
-    actual_arrival
-from
-    {{ source('demo_src', 'flights') }}
-{% if is_incremental() %}
-where scheduled_departure > (select max(scheduled_departure) - interval '100 day' from {{this}})
-{% endif %}
+with source as (
+
+    select * from {{ source('demo_src', 'flights') }}
+
+),
+
+renamed as (
+
+    select
+        flight_id,
+        flight_no,
+        scheduled_departure,
+        scheduled_arrival,
+        departure_airport,
+        arrival_airport,
+        status,
+        aircraft_code,
+        actual_departure,
+        actual_arrival
+
+    from source
+
+)
+
+select * from renamed
--- models/staging/flights/stg_flights__ticket_flights.sql
+++ models/staging/flights/stg_flights__ticket_flights.sql
@@ -1,13 +1,19 @@
-{{
-  config(
-    materialized = 'table',
-    tag = 'staging'
-    )
-}}
+with source as (
 
-select
-  ticket_no,
-  flight_id,
-  fare_conditions,
-  amount
-from {{ source('demo_src', 'ticket_flights') }}
+    select * from {{ source('demo_src', 'ticket_flights') }}
+
+),
+
+renamed as (
+
+    select
+        ticket_no,
+        flight_id,
+        fare_conditions,
+        amount
+
+    from source
+
+)
+
+select * from renamed
