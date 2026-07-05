a3c586d8c27a02c73c30610799f88b98475047ce
Maxim Egorov <maxim.egorov@fundraiseup.com>
2026-07-05 15:02:17 +0100

    vault commit

---
+++ models/raw_stage/raw_airports.sql
@@ -0,0 +1,17 @@
+{{
+  config(
+    materialized = 'table',
+    tag = 'staging'
+    )
+}}
+
+select
+  airport_code,
+  airport_name,
+  city,
+  coordinates,
+  timezone,
+  'bookings' as RECORD_SOURCE,
+  now() as LOAD_DATE
+from {{ source('demo_src', 'airports') }}
+    
